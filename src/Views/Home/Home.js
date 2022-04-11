ed63489acb8cc8764a522f76459c6a028bc241c3
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-11 14:49:51 -0700

    fixed random index

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -2,10 +2,29 @@ import { useState } from 'react';
 
 export default function Home() {
   const [selected, setSelected] = useState(null);
+
   const [isFiltering, setIsFiltering] = useState(false);
-  const sampleData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'duck', 'goose', 'santa', 'andy'];
+
+  const [sampleData, setSampleData] = useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    'duck',
+    'goose',
+    'santa',
+    'andy',
+  ]);
+
   const randomIndex = (array) => {
-    return array[Math.floor(Math.random() * array.length)];
+    const randomNum = Math.floor(Math.random() * array.length);
+    return array[randomNum];
   };
   return (
     <div>
@@ -20,7 +39,6 @@ export default function Home() {
       )}
       <div className="restaurants-container">
         <p>Results: {sampleData.length}</p>
-
         <button onClick={() => setSelected(null)}>See All</button>
       </div>
       {selected ? <p>{selected}</p> : sampleData.map((data) => <p key={data}>{data}</p>)}
