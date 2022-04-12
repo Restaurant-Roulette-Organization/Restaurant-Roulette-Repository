de9efe2822590c69a8f64b8e4492d174c6bf40d5
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-12 09:11:25 -0700

    added routes for app and a basic profile page outline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Auth from './Views/Auth/Auth';
 // import { useRestaurantContext } from './Context/RestaurantContext';
 import Home from './Views/Home/Home';
+import Profile from './Views/Profile/Profile';
 
 function App() {
   // const { restaurants } = useRestaurantContext();
+  const user = true;
 
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Route exact path={'/'}>
+          <Home />
+        </Route>
+        <Route path={'/auth'}>
+          <Auth />
+        </Route>
+        <Route path={'/profile'}>{user ? <Profile /> : <Redirect to={'/auth'} />}</Route>
+      </BrowserRouter>
     </div>
   );
 }
