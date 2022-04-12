745f6b10d65424ed3fa87a767afa699d935f6fd7
Denver McCarthy <dmichaelspdx@gmail.com>
2022-04-12 10:05:55 -0700

    general css outline

--- src/Components/Restaurant-List/Restaurant-List.js
+++ src/Components/Restaurant-List/Restaurant-List.js
@@ -1,9 +1,20 @@
-export const RestaurantListItem = ({ name, rating, price }) => {
+import { useState } from 'react';
+import './restaurantlist.css';
+
+export const RestaurantListItem = ({ name, rating, price, image_url }) => {
+  const [checked, setChecked] = useState(false);
   return (
-    <div>
-      <h3>{name}</h3> 
-      <p>{Array(Math.floor(rating)).fill('⭐️')}</p>
-      <p>{price}</p>
+    <div className="card">
+      <div className="restaurant-image" style={{ backgroundImage: `url(${image_url})` }}></div>
+
+      <p className="price">{price}</p>
+      <h3 className="title">{name}</h3>
+      <div className="stars-heart">
+        <p className="stars">{Array(Math.floor(rating)).fill('⭐️')}</p>
+        <div className="favorite" onClick={() => setChecked(!checked)}>
+          {checked ? '❤️' : '🤍'}
+        </div>
+      </div>
     </div>
   );
 };
