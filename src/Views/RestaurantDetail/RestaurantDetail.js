ac8893892b4da191fd353a30f866ccd3c450655b
Jacegk1999 <Jacegk1999@gmail.com>
2022-04-12 17:23:46 -0700

    setup outline for what we want details to look like

--- src/Views/RestaurantDetail/RestaurantDetail.js
+++ src/Views/RestaurantDetail/RestaurantDetail.js
@@ -2,7 +2,30 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function RestaurantDetail() {
-  //   const { alias } = useParams();
-  // const {restaurants}
-  return <div>Restaurant-Detail</div>;
+  const RestaurantDetail = {
+    name,
+    image_url,
+    rating,
+    price,
+    url,
+    location,
+    phoneNumber,
+    checked,
+    notes,
+  };
+
+  return (
+    <div>
+      <h3 className="title">{name}</h3>
+      <div className="restaurant-image" style={{ backgroundImage: `url(${image_url})` }}></div>
+      <p className="price">{price}</p>
+      <p>{rating}</p>
+      <p>{location}</p>
+      <p>{phoneNumber}</p>
+      <div className="favorite" onClick={() => clickHandler()}>
+        {checked ? '❤️' : '🤍'}
+      </div>
+      <div>{notes}</div>
+    </div>
+  );
 }
